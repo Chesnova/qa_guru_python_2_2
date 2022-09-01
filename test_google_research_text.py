f5728d4f4ddaaa3f859524427a16f5f56189906e
Chesnova <myjobistester@gmail.com>
2022-09-01 22:46:29 +0300

    add new description

--- test_google_research_text.py
+++ test_google_research_text.py
@@ -6,10 +6,10 @@ import pytest
 def open_browser():
     browser.open('https://google.com/ncr')
 
-def test_should_find_element(open_browser):
+def test_should_find_text(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_shouldnt_find_element(open_browser):
+def test_shouldnt_find_text(open_browser):
     browser.element('[name="q"]').should(be.blank).type('slkrtetw').press_enter()
     browser.element('[id="search"]').should(have.no.text('Selene - User-oriented Web UI browser tests in Python'))
